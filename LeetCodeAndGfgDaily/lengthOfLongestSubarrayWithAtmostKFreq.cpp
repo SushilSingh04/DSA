df3c8e3ad5daacd728a7b3b8f60af9fe6cbff566
Sushil <sushilksingh2001@gmail.com>
2026-08-13 09:59:48 +0530

    12-08-26 LC

---
+++ LeetCodeAndGfgDaily/lengthOfLongestSubarrayWithAtmostKFreq.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxSubarrayLength(vector<int>& nums, int k) {
+        int n = nums.size();
+        unordered_map<int, int> mp;
+        int i = 0, maxLength = INT_MIN;
+        
+        for(int j = 0; j < n; j++){
+            mp[nums[j]]++;
+            while(mp[nums[j]] > k){
+                mp[nums[i]]--;
+                i++;
+            }
+            maxLength = max(maxLength, j-i+1); 
+        }
+
+        return maxLength;
+    }
+};
