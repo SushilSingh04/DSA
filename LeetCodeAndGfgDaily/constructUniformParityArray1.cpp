4867357e8120d7fc55056795ea7d0bb86bf7bfbe
Sushil <sushilksingh2001@gmail.com>
2026-09-02 19:25:00 +0530

    02-09-26 LC

---
+++ LeetCodeAndGfgDaily/constructUniformParityArray1.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        // case 1: if all even then return true;
+        // case 2: if atleast one odd. And even - odd = odd, we can convert all evens to odds therfore reaching out goal
+       return true;
+    }
+};
