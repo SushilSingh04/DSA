f92f66fa733acf99162e0c3b4b940399f8b7cab4
Sushil <sushilksingh2001@gmail.com>
2026-09-04 12:12:21 +0530

    04-09-26 LC

---
+++ LeetCodeAndGfgDaily/smallestStableIndex.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> prefixMax(n,0);
+        prefixMax[0] = nums[0];
+
+        for(int i = 1; i < n; i++){
+            prefixMax[i] = max(prefixMax[i-1], nums[i]);
+        }
+        
+        vector<int> suffixMin(n,0);
+        suffixMin[n-1] = nums[n-1];
+
+        for(int i = n-2; i >= 0; i--){
+            suffixMin[i] = min(suffixMin[i+1], nums[i]);
+        }
+
+        int instability = INT_MAX;
+        for(int i = 0; i < n; i++){
+            instability = prefixMax[i] - suffixMin[i];
+            if(instability <= k) return i;
+        }
+
+        return -1;
+    }
+};
